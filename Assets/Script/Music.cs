4e07e2985eb54358f0324138d361124da26dbbc6
EmBaoToiChoi <hoaibao200611423@gmail.com>
2025-07-12 21:11:44 +0700

    Music

--- Assets/Script/Music.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[System.Serializable]
-public class Music : MonoBehaviour
-{
-    public string Name;
-    public AudioClip music;
-}
