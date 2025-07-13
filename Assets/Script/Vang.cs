00642e8deec1f19d806d16e9acf95c343edf28d0
EmBaoToiChoi <hoaibao200611423@gmail.com>
2025-07-13 21:33:02 +0700

    1

--- Assets/Script/Vang.cs
+++ Assets/Script/Vang.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Vang : MonoBehaviour
 {
     [SerializeField] private TMP_Text scoreText;
