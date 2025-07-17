e1de5b7a103d88516c253773c25d1aaa6235856b
EmBaoToiChoi <hoaibao200611423@gmail.com>
2025-07-17 18:28:40 +0700

    1

--- Assets/Script/Trap_X.cs
+++ Assets/Script/Trap_X.cs
@@ -2,7 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Trap_X : MonoBehaviour
+public class TRAP_X : MonoBehaviour
 {
+    public float start, end;  
+    public float speed = 2f;  
+    private bool movingRight = true; 
+
     
+
+    void Update()
+    {
+        float posX = transform.position.x;
+
+        
+        if (posX > end) movingRight = false;
+        if (posX < start) movingRight = true;
+
+      
+        Vector2 direction = movingRight ? Vector2.right : Vector2.left;
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
 }
