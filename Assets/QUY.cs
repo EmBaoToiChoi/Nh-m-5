26397f644fc10ab25f383cea34b6f3536ac0fb76
EmBaoToiChoi <hoaibao200611423@gmail.com>
2025-07-27 14:46:34 +0700

    1

--- Assets/QUY.cs
+++ Assets/QUY.cs
@@ -24,8 +24,8 @@ public class QUY : MonoBehaviour
     private Transform mainCam;
 
     private bool isChasing = false;
-    private float speed = 2f;
-    private float PVipHien = 10f;
+    private float speed = 4f;
+    private float PVipHien = 15f;
 
     private float maxHealth = 80f;
     private float currentHealth;
