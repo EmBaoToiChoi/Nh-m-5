b930173aa4da15aa68ca95f1613d7cd2e7c18301
EmBaoToiChoi <hoaibao200611423@gmail.com>
2025-08-01 15:51:21 +0700

    full đổi NV

--- Assets/PlayerSpawner.cs
+++ Assets/PlayerSpawner.cs
@@ -3,26 +3,30 @@ using Cinemachine;
 
 public class PlayerSpawner : MonoBehaviour
 {
-    public GameObject[] players; // Prefab 3 nhân vật
-    public Transform spawnPoint;
-    public CinemachineVirtualCamera mainCamera;
-    public Transform minimapCamera; // Camera minimap (gán bằng tay hoặc tìm qua tag)
+  
+
+    public GameObject[] players; // Gán 3 RF nhân vật (đã tắt hết)
+    public Cinemachine.CinemachineVirtualCamera mainCamera;
+    public Transform minimapCamera;
 
     void Start()
     {
         int selected = PlayerPrefs.GetInt("SelectedCharacter", 0);
 
-        // Spawn nhân vật
-        GameObject player = Instantiate(players[selected], spawnPoint.position, Quaternion.identity);
+        for (int i = 0; i < players.Length; i++)
+        {
+            players[i].SetActive(i == selected); // Chỉ bật đúng nhân vật
+        }
 
-        // Gán player cho camera chính
+        GameObject player = players[selected];
+
+        // Gán camera follow
         mainCamera.Follow = player.transform;
         mainCamera.LookAt = player.transform;
 
-        // Gán player cho minimap
-        if (minimapCamera != null)
-        {
-            minimapCamera.GetComponent<MinimapFollow>().target = player.transform;
-        }
+        // Gán minimap follow
+        minimapCamera.GetComponent<MinimapFollow>().target = player.transform;
     }
+
+
 }
